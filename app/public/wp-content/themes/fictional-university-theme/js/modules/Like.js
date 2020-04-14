17b53d44e1e89fad19abe3b3cd17851ebb8c287d
ahmer hayatt <khusoo.hayat@gmail.com>
2020-04-14 20:55:27 +0500

    20.3 3. Creating Custom POST and DELETE Endpoints

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Like.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Like.js
@@ -1,33 +1,50 @@
 import $ from 'jquery';
 
 class Like {
-    constructor() {
-        this.events();
+  constructor() {
+    this.events();
+  }
+
+  events() {
+    $(".like-box").on("click", this.ourClickDispatcher.bind(this));
+  }
+
+  // methods
+  ourClickDispatcher(e) {
+    var currentLikeBox = $(e.target).closest(".like-box");
+
+    if (currentLikeBox.data('exists') == 'yes') {
+      this.deleteLike();
+    } else {
+      this.createLike();
     }
+  }
 
-    events() {
-        $(".like-box").on("click", this.ourClickDispatcher.bind(this));
-    } 
+  createLike() {
+    $.ajax({
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      type: 'POST',
+      success: (response) => {
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    });
+  }
 
-    // methods
-    ourClickDispatcher(e) {
-        alert('here');
-        var currentLikeBox = $(e.target).closest(".like-box");
-
-        if(currentLikeBox.data('exists' == 'yes')) {
-            this.deleteLike();
-        } else {
-            this.createLike();
-        }
-    } 
-
-    createLike() {
-        alert("create test message");
-    }
-
-    deleteLike() {
-        alert("delete test message");
-    }
+  deleteLike() {
+    $.ajax({
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      type: 'DELETE',
+      success: (response) => {
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    });
+  }
 }
 
 export default Like;
