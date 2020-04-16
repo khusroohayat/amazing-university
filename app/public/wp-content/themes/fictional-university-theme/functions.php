79f5e28f8dd98e513f369e1defcef49d8315e8c0
ahmer hayatt <khusoo.hayat@gmail.com>
2020-04-16 19:52:49 +0500

    final changes

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -52,7 +52,7 @@ function pageBanner($args = NULL) {
   </div>
 <?php }
 function university_files() {
-    wp_enqueue_script('googleMap', '//maps.google.com/maps/api/js?key=AIzaSyBSDETFcCM0kzVBRSg0b0j43rEYiZYNMHM', NULL, '1.0', true);
+    wp_enqueue_script('googleMap', '//maps.google.com/maps/api/js?key=AIzaSyBh9b1rNCp6kOi5JeMHiRP4klDymBeoEWk', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
